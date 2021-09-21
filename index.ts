b8956929a09404d4b3d43c8eeaab1481ccd4a25f
Josias Aurel <ndjosiasaurel@gmail.com>
2021-09-21 23:53:29 +0000

    export generated imaegW image

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from "express";
 import puppeteer from "puppeteer";
 import { writeFileSync, unlinkSync } from "fs";
 import { nanoid } from "nanoid";
+import { screenshot } from "./config";
 
 const app: Application = express();
 
@@ -9,23 +10,15 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Page Screenshot API ready");
 });
 
-app.get("/screenshot", (req: Request, res: Response) => {
+app.get("/screenshot", async (req: Request, res: Response) => {
 
     // get the queried page
-    const webPage: any = req.query.page;
-    const randomImageName: string = nanoid(6);
+    const webPage: any = await req.query.page;
+    const randomImageName: string = await nanoid(6);
     
     // initialize and execute puppeteer screenshot
-    (async () => {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto(webPage);
-        await page.screenshot({ path: `/tmp/${randomImageName}.png` });
-        await browser.close(); // close browser instance
-
-        // respond with the generated image
-        await res.sendFile(`/tmp/${randomImageName}.png`);
-      })().then(_ => {
+    screenshot(webPage, randomImageName).then(_ => {
+        res.sendFile(`/tmp/${randomImageName}.png`);
         // delete the image
         unlinkSync(`/tmp/${randomImageName}.png`);
       });
